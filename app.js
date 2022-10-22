8fa0947948b6d6f0e7027eb7210761a02d5dc3fa
Daniela Liendo <danielaliendo98@gmail.com>
2022-10-22 19:39:09 -0300

    .env file added

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
+require('dotenv').config();
 const { engine } = require('express-handlebars');
 const express = require('express');
-var path = require('path')
+var path = require('path');
 
 const app = express();
+const port = process.env.PORT
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.engine('handlebars', engine());
@@ -13,14 +16,12 @@ app.get('/', (req, res) => {
   res.render('home');
 });
 
-
 app.get('/elements', (req, res) => {
   res.render('elements');
 });
-
 
 app.get('/generic', (req, res) => {
   res.render('generic');
 });
 
-app.listen(8080);
+app.listen(port);
