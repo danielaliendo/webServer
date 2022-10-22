0945c61bdd5d7fd0af6316b906a7f17d3211c535
Daniela Liendo <danielaliendo98@gmail.com>
2022-10-22 10:09:09 -0300

    express initial configuration was added

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const app = express()
+
+const port = 8080
+
+app.get('/', function (req, res) {
+  res.send('Hello World')
+})
+
+app.get('*', function (req, res) {
+  res.send('404 | Page not found')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
