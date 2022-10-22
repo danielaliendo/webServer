7038d6617f64ab594053425d40f0f6ec9f77a0f9
Daniela Liendo <danielaliendo98@gmail.com>
2022-10-22 16:06:00 -0300

    index and 404 html files page were added and served statically

--- app.js
+++ app.js
@@ -3,12 +3,15 @@ const app = express()
 
 const port = 8080
 
+// Serve static content
+app.use(express.static('public'));
+
 app.get('/', function (req, res) {
   res.send('Hello World')
 })
 
 app.get('*', function (req, res) {
-  res.send('404 | Page not found')
+  res.sendFile(__dirname + '/public/404.html')
 })
 
 app.listen(port, () => {
